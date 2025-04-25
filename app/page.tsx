e148cce52e85b378fc4bc933ba5e3bee1d8a2177
Hamzat Malik <hamzatmalik20@yahoo.com>
2025-04-25 01:03:38 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-export const metadata = {
-  other: {
-    'fc:frame': 'vNext',
-    'fc:frame:image': 'https://i.imgur.com/0jD5WVs.png',
-    'fc:frame:button:1': 'Show TL;DRs',
-    'fc:frame:button:2': 'Top 5 Trending',
-    'fc:frame:button:3': 'Base TL;DR',
-    'fc:frame:post_url': 'https://tldr-drop.vercel.app/api/frame',
-    'og:title': '@captainsnowboy’s Weekly TL;DR Drop',
-    'og:image': 'https://i.imgur.com/0jD5WVs.png',
-  },
-};
-
 export default function Home() {
   return (
     <main>
