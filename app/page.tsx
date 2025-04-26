4b15ec9a3e1cc02dea23926301e4a51d15006964
Hamzat Malik <hamzatmalik20@yahoo.com>
2025-04-26 18:38:22 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <>
       <Head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://i.imgur.com/0jD5WVs.png" />
+        <meta property="fc:frame:image" content="https://i.imgur.com/3vDIVvk.jpeg" />
         <meta property="fc:frame:button:1" content="Show TL;DRs" />
         <meta property="fc:frame:button:2" content="Top 5 Trending" />
         <meta property="fc:frame:button:3" content="Base TL;DR" />
         <meta property="fc:frame:post_url" content="https://tldr-drop.vercel.app/api/frame" />
         <meta property="og:title" content="@captainsnowboy’s Weekly TL;DR Drop" />
-        <meta property="og:image" content="https://i.imgur.com/0jD5WVs.png" />
+        <meta property="og:image" content="https://i.imgur.com/3vDIVvk.jpeg" />
         <title>@captainsnowboy’s Weekly TL;DR Drop</title>
       </Head>
       <main>
